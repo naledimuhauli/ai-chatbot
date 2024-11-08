3d937678def2f4ddcefdd9c406416e2c15b1abbd
siphokazi <naledimuhauli@gmail.com>
2024-11-08 03:26:07 +0200

    installed icons

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import ChatIcon from '@mui/icons-material/Chat';
+import GroupIcon from '@mui/icons-material/Group';
+import ExtensionIcon from '@mui/icons-material/Extension';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h2>AI Chat App</h2>
             <ul>
-                <li>Home</li>
-                <li>Profile</li>
-                <li>Settings</li>
+                <li><ChatIcon /> Chat</li>
+                <li><GroupIcon /> Members</li>
+                <li><ExtensionIcon /> Integrations</li>
+                <li><PersonAddIcon /> Refer Friends</li>
+                <li><AttachMoneyIcon /> Pricing Plans</li>
+                <li><SettingsIcon /> Settings</li>
             </ul>
         </div>
     );
