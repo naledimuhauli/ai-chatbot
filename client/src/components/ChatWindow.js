755a3c13c56b46d2930fd7dd9968f5fb27a83960
siphokazi <naledimuhauli@gmail.com>
2024-11-09 23:04:04 +0200

    styled the search history section

--- client/src/components/ChatWindow.js
+++ client/src/components/ChatWindow.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';  // Import the CSS file for styling
+import './style.css';
 import Message from './Message';
 import Sidebar from './sidebar';
 import InputField from './InputField';
+import MessageIcon from '@mui/icons-material/Message'; // Import the message icon
 
 function ChatWindow() {
     const [messages, setMessages] = useState([]);
@@ -24,6 +25,16 @@ function ChatWindow() {
                 <div className="navbar">
                     <p className="name">Johnson Doe</p>
                     <hr />
+                </div>
+                <div className="blank">
+                    <p className="answer">Get answers in seconds</p>
+                    <span className="text">Create and complete tasks using boards</span>
+                    <div className="serach-history">
+                        <span>Search history</span><span>Clear Chat History</span>
+                        <MessageIcon className="message-icon" /> {/* Add the message icon */}
+                        <h3>No Questions added</h3>
+                        <p>Type your questions below and get answers fast</p>
+                    </div>
                 </div>
                 <div className="messages">
                     {messages.map((msg, index) => (
